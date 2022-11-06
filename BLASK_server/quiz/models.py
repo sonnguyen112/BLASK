b6496d1d71d392a8441adcccc9ab7d14fa389dbd
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2022-11-06 22:16:15 +0700

    fix_bug

--- BLASK_server/quiz/models.py
+++ BLASK_server/quiz/models.py
@@ -1,4 +1,3 @@
-from tkinter import TRUE
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
@@ -6,21 +5,21 @@ import uuid
 class Quiz(models.Model):
     createAt = models.DateTimeField(auto_now_add = True)
     updateAt = models.DateTimeField(auto_now = True)
-    title = models.CharField(max_length=100, null = True)#null
+    title = models.CharField(max_length=100)
     description = models.TextField(null = True, blank = True)
-    userOf = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    userOf = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Question(models.Model):
     description = models.TextField(null = True, blank = True)
     time = models.TimeField()
-    image = models.FileField(upload_to= "QuestionImage", blank = True)
+    image = models.ImageField(upload_to= "QuestionImage", blank = True, null = True)
     score = models.IntegerField()
-    quizOf = models.ForeignKey(Quiz, on_delete=models.CASCADE, null = True )
-    numberOfAnswer= models.IntegerField(default = 0)
+    quizOf = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    numberOfAnswer= models.IntegerField(default = 1)
 
 class Option(models.Model):
     content =  models.TextField(null = True, blank = True)
-    image = models.FileField(upload_to= "OptionImage", blank = True)
-    questionOf = models.ForeignKey(Question, on_delete=models.CASCADE, null= True )
+    image = models.FileField(upload_to= "OptionImage", blank = True, null = True)
+    questionOf = models.ForeignKey(Question, on_delete=models.CASCADE)
     isTrue = models.BooleanField(default=False)
 
