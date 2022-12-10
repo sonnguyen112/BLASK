4d5b65a88f682767045a73a85c2d5eaff2bb19ee
htrbao <trbaohoo@gmail.com>
2022-12-10 15:40:02 +0700

    my minigame

--- frontend/src/components/minigame/Game.js
+++ frontend/src/components/minigame/Game.js
@@ -1,36 +1,45 @@
 import React, { useState } from "react"
 import Board from "./Board"
 import "./GameStyles.css"
+import AviGame from "./The-Aviator/js/AviGame"
 import { checkWin } from "./Utils"
 
-const Game = () => {
-    const [board, setBoard] = useState(Array(9).fill(null))
-    const [xIsNext, setXIsNext] = useState(true);
-    const winner = checkWin(board)
-    const handleClick = (index) => {
-        const copyBoard = [...board];
-        if (winner || copyBoard[index]) return;
-        copyBoard[index] = xIsNext ? 'X' : 'O';
-        setBoard(copyBoard);
-        setXIsNext(!xIsNext);
-    };
-    const handleResetGame = () => {
-        setBoard(Array(9).fill(null));
-        setXIsNext(true);
-    }
-    let tcolor = (!xIsNext ? "blue" : "red");
-    return (
-        <div className="blask-minigame">
-            <div className="game" id={tcolor}>
-                <Board cells={board} onClick={handleClick}>
+// const Game = () => {
+//     const [board, setBoard] = useState(Array(9).fill(null))
+//     const [xIsNext, setXIsNext] = useState(true);
+//     const winner = checkWin(board)
+//     const handleClick = (index) => {
+//         const copyBoard = [...board];
+//         if (winner || copyBoard[index]) return;
+//         copyBoard[index] = xIsNext ? 'X' : 'O';
+//         setBoard(copyBoard);
+//         setXIsNext(!xIsNext);
+//     };
+//     const handleResetGame = () => {
+//         setBoard(Array(9).fill(null));
+//         setXIsNext(true);
+//     }
+//     let tcolor = (!xIsNext ? "blue" : "red");
+//     return (
+//         <div className="blask-minigame">
+//             <div className="game" id={tcolor}>
+//                 <Board cells={board} onClick={handleClick}>
 
-                </Board>
-                {winner ? `winner is ${xIsNext ? 'O' : 'X'}` : ""}
+//                 </Board>
+//                 {winner ? `winner is ${xIsNext ? 'O' : 'X'}` : ""}
                 
-                <button className="blask-minigame-reset" onClick={handleResetGame}>Reset Now</button>
-            </div>
+//                 <button className="blask-minigame-reset" onClick={handleResetGame}>Reset Now</button>
+//             </div>
+//         </div>
+//     );
+// };
+
+const Game = () => {
+    return (
+        <div className=".blask-minigame">
+            <AviGame></AviGame>
         </div>
-    );
-};
+    )
+}
 
 export default Game
---
+++ frontend/src/components/minigame/The-Aviator/js/AviGame.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/styles.css"
+
+const AviGame = () => {
+    return (
+        <div id="world">
+            
+        </div>
+    );
+};
+
+export default AviGame
