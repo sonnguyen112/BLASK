afd8361f9db3d45ee8f65fe9e083f9bc6ea1b59f
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2022-12-19 20:26:18 +0700

    cors

--- BLASK_server/BLASK_server/settings.py
+++ BLASK_server/BLASK_server/settings.py
@@ -41,6 +41,7 @@ MEDIA_URL = '/media/'
 
 INSTALLED_APPS = [
     "daphne",
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -158,3 +160,7 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'BLASK/mediafiles')
 
 # URL used to access the media
 MEDIA_URL = '/media/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
