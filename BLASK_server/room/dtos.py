d0ab63b5eee839b401a33759941b6d3897e90030
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2022-12-31 01:00:31 +0700

    commit

--- BLASK_server/room/dtos.py
+++ BLASK_server/room/dtos.py
@@ -1,11 +1,42 @@
 from rest_framework.authtoken.models import Token
 
 
-class GetQuizDTO():
-    def __init__(self, quiz, list_question, list_option) -> None:
-        self.title = quiz.title
-        self.description = quiz.description
-        self.imageQuizUrl = quiz.imageQuizUrl
+# class GetQuizDTO():
+#     def __init__(self, quiz, list_question, list_option) -> None:
+#         self.title = quiz.title
+#         self.description = quiz.description
+#         self.imageQuizUrl = quiz.imageQuizUrl
+#         self.list_question = self.parse_list_question(list_question)
+#         self.list_option = self.parse_list_option(list_option)
+
+#     def parse_list_question(self, list_question):
+#         parse_list_question = []
+#         for question in list_question:
+#             parse_list_question.append({
+#                 "id": question.id,
+#                 "description": question.description,
+#                 "num_of_second": question.numOfSecond,
+#                 "image_question_url": question.imageQuestionUrl,
+#                 "score": question.score
+#             })
+#         return parse_list_question
+
+#     def parse_list_option(self, list_option):
+#         parse_list_option = []
+#         for option in list_option:
+#             parse_list_option.append({
+#                 "id": option.id,
+#                 "question": option.questionOf.id,
+#                 "content": option.content,
+#                 "image_option_url": option.imageOptionUrl
+#             })
+#         return parse_list_option
+
+
+class CreateRoomDTO():
+    def __init__(self, room, list_question, list_option) -> None:
+        self.pin = room.pin
+        self.token_host = Token.objects.get(user=room.host).key
         self.list_question = self.parse_list_question(list_question)
         self.list_option = self.parse_list_option(list_option)
 
@@ -33,12 +64,31 @@ class GetQuizDTO():
         return parse_list_option
 
 
-class CreateRoomDTO():
-    def __init__(self, room) -> None:
-        self.pin = room.pin
-        self.token_host = Token.objects.get(user=room.host).key
-
-
 class JoinRoomDTO():
-    def __init__(self, room) -> None:
+    def __init__(self, room, list_question, list_option) -> None:
         self.token_host = Token.objects.get(user=room.host).key
+        self.list_question = self.parse_list_question(list_question)
+        self.list_option = self.parse_list_option(list_option)
+
+    def parse_list_question(self, list_question):
+        parse_list_question = []
+        for question in list_question:
+            parse_list_question.append({
+                "id": question.id,
+                "description": question.description,
+                "num_of_second": question.numOfSecond,
+                "image_question_url": question.imageQuestionUrl,
+                "score": question.score
+            })
+        return parse_list_question
+
+    def parse_list_option(self, list_option):
+        parse_list_option = []
+        for option in list_option:
+            parse_list_option.append({
+                "id": option.id,
+                "question": option.questionOf.id,
+                "content": option.content,
+                "image_option_url": option.imageOptionUrl
+            })
+        return parse_list_option
