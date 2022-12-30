b183c8eb7053b964e71179f7970597dc83824e50
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2022-12-31 01:15:45 +0700

    commit

--- BLASK_server/room/models.py
+++ BLASK_server/room/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from quiz import Quiz
+from quiz.models import Quiz
 
 # Create your models here.
 class Room(models.Model):
