6f9c485767408a296fb7079c737231b66e1d287f
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2023-01-03 16:17:41 +0700

    commit

--- BLASK_server/BLASK_server/settings.py
+++ BLASK_server/BLASK_server/settings.py
@@ -165,7 +165,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
     "http://localhost:3000",
     "https://67fd-113-172-80-141.ap.ngrok.io",
-    "http://27.74.18.72",
+    "http://27.74.18.72/",
     "http://blaskweb.ddns.net",
     "https://blaskweb.ddns.net"
 ]
