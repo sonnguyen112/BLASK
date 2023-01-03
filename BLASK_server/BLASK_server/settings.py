e4b2911ac7eff7c917a7752ffc72ade21e2313bf
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2023-01-03 12:19:34 +0700

    commit

--- BLASK_server/BLASK_server/settings.py
+++ BLASK_server/BLASK_server/settings.py
@@ -165,5 +165,6 @@ MEDIA_URL = '/media/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
     "http://localhost:3000",
-    "https://67fd-113-172-80-141.ap.ngrok.io"
+    "https://67fd-113-172-80-141.ap.ngrok.io",
+    "http://27.74.18.72/"
 ]
