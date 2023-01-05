364885deba9dfed98259c7ef87027ae2e60a985c
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2023-01-05 15:50:27 +0700

    fix_bug

--- BLASK_server/room/consumers.py
+++ BLASK_server/room/consumers.py
@@ -1,62 +1,58 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
-from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer
 from .utils import check_answer
 
 
-class WaitConsumer(WebsocketConsumer):
-    def connect(self):
+class WaitConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "room_%s" % self.room_name
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
+       await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
 
     # Receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         name_player = text_data_json['name_player']
         avatar = text_data_json['avatar']
         type_action = text_data_json["type_action"]
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name, {"type": "wait_message", "name_player": name_player, "avatar": avatar,
                                    "type_action": type_action}
         )
 
     # Receive message from room group
-    def wait_message(self, event):
+    async def wait_message(self, event):
         name_player = event["name_player"]
         avatar = event["avatar"]
         type_action = event["type_action"]
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'name_player': name_player,
             'avatar': avatar,
             "type_action": type_action
         }))
 
 
-class PlayConsumer(WebsocketConsumer):
+class PlayConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "room_%s" % self.room_name
 
         # Join room group
-        await async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
         )
 
@@ -64,7 +60,7 @@ class PlayConsumer(WebsocketConsumer):
 
     async def disconnect(self, close_code):
         # Leave room group
-        await async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
 
@@ -78,7 +74,7 @@ class PlayConsumer(WebsocketConsumer):
             option_id_player_choose = text_data_json['option_id_player_choose']
             remaining_time = text_data_json['remaining_time']
             # Send message to room group
-            await async_to_sync(self.channel_layer.group_send)(
+            await self.channel_layer.group_send(
                 self.room_group_name, {"type": "play_message", "name_player": name_player,
                                        "question_id": question_id, "option_id_player_choose": option_id_player_choose,
                                        "remaining_time": remaining_time,
@@ -86,7 +82,7 @@ class PlayConsumer(WebsocketConsumer):
             )
         elif type_action == "score_board":
             # Send message to room group
-            await async_to_sync(self.channel_layer.group_send)(
+            await self.channel_layer.group_send(
                 self.room_group_name, {
                     "type": "play_message",
                     "type_action": type_action
@@ -95,7 +91,7 @@ class PlayConsumer(WebsocketConsumer):
         elif type_action == "next":
             index_next_ques = text_data_json["index_next_ques"]
             # Send message to room group
-            await async_to_sync(self.channel_layer.group_send)(
+            await self.channel_layer.group_send(
                 self.room_group_name, {
                     "type": "play_message",
                     "type_action": type_action,
@@ -104,7 +100,7 @@ class PlayConsumer(WebsocketConsumer):
             )
         elif type_action == "delete":
             # Send message to room group
-            await async_to_sync(self.channel_layer.group_send)(
+            await self.channel_layer.group_send(
                 self.room_group_name, {
                     "type": "play_message",
                     "type_action": type_action
@@ -114,7 +110,7 @@ class PlayConsumer(WebsocketConsumer):
             name_player = text_data_json['name_player']
             avatar = text_data_json["avatar"]
             # Send message to room group
-            await async_to_sync(self.channel_layer.group_send)(
+            await self.channel_layer.group_send(
                 self.room_group_name, {
                     "type": "play_message",
                     "type_action": type_action,
@@ -125,7 +121,7 @@ class PlayConsumer(WebsocketConsumer):
         elif type_action == "timeout":
             name_player = text_data_json['name_player']
             # Send message to room group
-            await async_to_sync(self.channel_layer.group_send)(
+            await self.channel_layer.group_send(
                 self.room_group_name, {
                     "type": "play_message",
                     "type_action": type_action,
