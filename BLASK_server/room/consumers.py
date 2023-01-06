4ba67054e12c5370a82700e60f161a2bce0a93d2
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2023-01-07 04:26:56 +0700

    commit

--- BLASK_server/room/consumers.py
+++ BLASK_server/room/consumers.py
@@ -23,60 +23,31 @@ class WaitConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        name_player = text_data_json['name_player']
-        avatar = text_data_json['avatar']
         type_action = text_data_json["type_action"]
-
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name, {"type": "wait_message", "name_player": name_player, "avatar": avatar,
-                                   "type_action": type_action}
-        )
-
-    # Receive message from room group
-    async def wait_message(self, event):
-        name_player = event["name_player"]
-        avatar = event["avatar"]
-        type_action = event["type_action"]
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'name_player': name_player,
-            'avatar': avatar,
-            "type_action": type_action
-        }))
-
-
-class PlayConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = "room_%s" % self.room_name
-
-        # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name, self.channel_name
-        )
-
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name, self.channel_name
-        )
-
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        type_action = text_data_json["type_action"]
-        if type_action == "answer":
+        if type_action == "append":
+            name_player = text_data_json['name_player']
+            avatar = text_data_json['avatar']
+            # Send message to room group
+            await self.channel_layer.group_send(
+                self.room_group_name, {"type": "wait_message", "name_player": name_player, "avatar": avatar,
+                                       "type_action": type_action}
+            )
+        elif type_action == "delete":
+            name_player = text_data_json['name_player']
+            avatar = text_data_json['avatar']
+            # Send message to room group
+            await self.channel_layer.group_send(
+                self.room_group_name, {"type": "wait_message", "name_player": name_player, "avatar": avatar,
+                                       "type_action": type_action}
+            )
+        elif type_action == "answer":
             name_player = text_data_json['name_player']
             question_id = text_data_json['question_id']
             option_id_player_choose = text_data_json['option_id_player_choose']
             remaining_time = text_data_json['remaining_time']
             # Send message to room group
             await self.channel_layer.group_send(
-                self.room_group_name, {"type": "play_message", "name_player": name_player,
+                self.room_group_name, {"type": "wait_message", "name_player": name_player,
                                        "question_id": question_id, "option_id_player_choose": option_id_player_choose,
                                        "remaining_time": remaining_time,
                                        "type_action": type_action}
@@ -88,61 +59,63 @@ class PlayConsumer(AsyncWebsocketConsumer):
             score = text_data_json["score"]
             await self.channel_layer.group_send(
                 self.room_group_name, {
-                    "type": "play_message",
+                    "type": "wait_message",
                     "type_action": type_action,
                     "rank": rank,
                     "score": score,
                     "name_player": name_player
-                    }
+                }
             )
         elif type_action == "next":
             index_next_ques = text_data_json["index_next_ques"]
             # Send message to room group
             await self.channel_layer.group_send(
                 self.room_group_name, {
-                    "type": "play_message",
+                    "type": "wait_message",
                     "type_action": type_action,
                     "index_next_ques": index_next_ques
-                    }
-            )
-        elif type_action == "delete":
-            name_player = text_data_json['name_player']
-            # Send message to room group
-            await self.channel_layer.group_send(
-                self.room_group_name, {
-                    "type": "play_message",
-                    "type_action": type_action,
-                    "name_player": name_player
-                    }
-            )
-        elif type_action == "append":
-            name_player = text_data_json['name_player']
-            avatar = text_data_json["avatar"]
-            # Send message to room group
-            await self.channel_layer.group_send(
-                self.room_group_name, {
-                    "type": "play_message",
-                    "name_player": name_player,
-                    "avatar" : avatar,
-                    "type_action": type_action,
-                    }
+                }
             )
         elif type_action == "timeout":
             name_player = text_data_json['name_player']
             # Send message to room group
             await self.channel_layer.group_send(
                 self.room_group_name, {
-                    "type": "play_message",
+                    "type": "wait_message",
                     "type_action": type_action,
                     "name_player": name_player,
-                    }
+                }
+            )
+        elif type_action == "play":
+            await self.channel_layer.group_send(
+                self.room_group_name, {
+                    "type": "wait_message",
+                    "type_action": type_action,
+                }
             )
 
-
     # Receive message from room group
-    async def play_message(self, event):
+    async def wait_message(self, event):
         type_action = event["type_action"]
-        if type_action == "answer":
+        if type_action == "append":
+            name_player = event["name_player"]
+            avatar = event["avatar"]
+            # Send message to WebSocket
+            await self.send(text_data=json.dumps({
+                'name_player': name_player,
+                'avatar': avatar,
+                "type_action": type_action
+            }))
+        elif type_action == "delete":
+            name_player = event["name_player"]
+            avatar = event["avatar"]
+            # Send message to WebSocket
+            await self.send(text_data=json.dumps({
+                'name_player': name_player,
+                'avatar': avatar,
+                "type_action": type_action
+            }))
+        elif type_action == "answer":
             name_player = event["name_player"]
             question_id = event["question_id"]
             option_id_player_choose = event["option_id_player_choose"]
@@ -174,26 +147,164 @@ class PlayConsumer(AsyncWebsocketConsumer):
                 "type_action": type_action,
                 "index_next_ques": index_next_ques
             }))
-        elif type_action == "delete":
-            name_player = event["name_player"]
-            # Send message to WebSocket
-            await self.send(text_data=json.dumps({
-                "type_action": type_action,
-                "name_player": name_player,
-            }))
-        elif type_action == "append":
-            name_player = event["name_player"]
-            avatar = event["avatar"]
-            # Send message to WebSocket
-            await self.send(text_data=json.dumps({
-                "type_action": type_action,
-                "name_player": name_player,
-                "avatar": avatar
-            }))
         elif type_action == "timeout":
             name_player = event["name_player"]
             await self.send(text_data=json.dumps({
                 "type_action": type_action,
                 "name_player": name_player,
             }))
+        elif type_action == "play":
+            await self.send(text_data=json.dumps({
+                "type_action": type_action,
+            }))
 
+
+# class PlayConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+#         self.room_group_name = "room_%s" % self.room_name
+
+#         # Join room group
+#         await self.channel_layer.group_add(
+#             self.room_group_name, self.channel_name
+#         )
+
+#         await self.accept()
+
+#     async def disconnect(self, close_code):
+#         # Leave room group
+#         await self.channel_layer.group_discard(
+#             self.room_group_name, self.channel_name
+#         )
+
+#     # Receive message from WebSocket
+#     async def receive(self, text_data):
+#         text_data_json = json.loads(text_data)
+#         type_action = text_data_json["type_action"]
+#         if type_action == "answer":
+#             name_player = text_data_json['name_player']
+#             question_id = text_data_json['question_id']
+#             option_id_player_choose = text_data_json['option_id_player_choose']
+#             remaining_time = text_data_json['remaining_time']
+#             # Send message to room group
+#             await self.channel_layer.group_send(
+#                 self.room_group_name, {"type": "play_message", "name_player": name_player,
+#                                        "question_id": question_id, "option_id_player_choose": option_id_player_choose,
+#                                        "remaining_time": remaining_time,
+#                                        "type_action": type_action}
+#             )
+#         elif type_action == "score_board":
+#             # Send message to room group
+#             name_player = text_data_json['name_player']
+#             rank = text_data_json["rank"]
+#             score = text_data_json["score"]
+#             await self.channel_layer.group_send(
+#                 self.room_group_name, {
+#                     "type": "play_message",
+#                     "type_action": type_action,
+#                     "rank": rank,
+#                     "score": score,
+#                     "name_player": name_player
+#                     }
+#             )
+#         elif type_action == "next":
+#             index_next_ques = text_data_json["index_next_ques"]
+#             # Send message to room group
+#             await self.channel_layer.group_send(
+#                 self.room_group_name, {
+#                     "type": "play_message",
+#                     "type_action": type_action,
+#                     "index_next_ques": index_next_ques
+#                     }
+#             )
+#         elif type_action == "delete":
+#             name_player = text_data_json['name_player']
+#             # Send message to room group
+#             await self.channel_layer.group_send(
+#                 self.room_group_name, {
+#                     "type": "play_message",
+#                     "type_action": type_action,
+#                     "name_player": name_player
+#                     }
+#             )
+#         elif type_action == "append":
+#             name_player = text_data_json['name_player']
+#             avatar = text_data_json["avatar"]
+#             # Send message to room group
+#             await self.channel_layer.group_send(
+#                 self.room_group_name, {
+#                     "type": "play_message",
+#                     "name_player": name_player,
+#                     "avatar" : avatar,
+#                     "type_action": type_action,
+#                     }
+#             )
+#         elif type_action == "timeout":
+#             name_player = text_data_json['name_player']
+#             # Send message to room group
+#             await self.channel_layer.group_send(
+#                 self.room_group_name, {
+#                     "type": "play_message",
+#                     "type_action": type_action,
+#                     "name_player": name_player,
+#                     }
+#             )
+
+
+#     # Receive message from room group
+#     async def play_message(self, event):
+#         type_action = event["type_action"]
+#         if type_action == "answer":
+#             name_player = event["name_player"]
+#             question_id = event["question_id"]
+#             option_id_player_choose = event["option_id_player_choose"]
+#             remaining_time = event["remaining_time"]
+#             is_true = await sync_to_async(check_answer)(question_id, option_id_player_choose)
+#             # Send message to WebSocket
+#             await self.send(text_data=json.dumps({
+#                 "type_action": type_action,
+#                 'name_player': name_player,
+#                 "question_id": question_id,
+#                 "is_true": is_true,
+#                 "remaining_time": remaining_time
+#             }))
+#         elif type_action == "score_board":
+#             # Send message to WebSocket
+#             name_player = event["name_player"]
+#             rank = event["rank"]
+#             score = event["score"]
+#             await self.send(text_data=json.dumps({
+#                 "type_action": type_action,
+#                 "name_player": name_player,
+#                 "rank": rank,
+#                 "score": score
+#             }))
+#         elif type_action == "next":
+#             index_next_ques = event["index_next_ques"]
+#             # Send message to WebSocket
+#             await self.send(text_data=json.dumps({
+#                 "type_action": type_action,
+#                 "index_next_ques": index_next_ques
+#             }))
+#         elif type_action == "delete":
+#             name_player = event["name_player"]
+#             # Send message to WebSocket
+#             await self.send(text_data=json.dumps({
+#                 "type_action": type_action,
+#                 "name_player": name_player,
+#             }))
+#         elif type_action == "append":
+#             name_player = event["name_player"]
+#             avatar = event["avatar"]
+#             # Send message to WebSocket
+#             await self.send(text_data=json.dumps({
+#                 "type_action": type_action,
+#                 "name_player": name_player,
+#                 "avatar": avatar
+#             }))
+#         elif type_action == "timeout":
+#             name_player = event["name_player"]
+#             await self.send(text_data=json.dumps({
+#                 "type_action": type_action,
+#                 "name_player": name_player,
+#             }))
--- frontend/src/pages/Room.js
+++ frontend/src/pages/Room.js
@@ -1,21 +1,274 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "../style/room.css";
-import {useLocation} from 'react-router-dom';
+import { useLocation, useNavigate } from "react-router-dom";
 import WaitingRoom from "../components/WaitingRoom";
+import PlayingRoom from "../components/PlayingRoom";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+const memberHandler = (
+  message,
+  pin,
+  quiz_info,
+  token_me,
+  token_host,
+  navigate,
+  client,
+  member,
+  setMember,
+  submit,
+  setSubmit,
+  setTypeRoom,
+  data,
+  setIndexQues,
+  setRankScore
+) => {
+  console.log("1", message);
+  let tmp_message = JSON.parse(message.data);
+
+  const onDelete = (name_player) => {
+    console.log(name_player);
+    let index = [...member].findIndex((m) => m.name_player === name_player);
+    if (index !== -1) {
+      const copyMember = [...member];
+      copyMember.splice(index, 1);
+      setMember(copyMember);
+    }
+
+    if (token_host !== token_me && name_player === token_me) {
+      client.close();
+      navigate("/");
+    }
+  };
+  const onAppend = (name_player, avatar) => {
+    let newmessage = {
+      name_player: name_player,
+      avatar: avatar,
+      score: 0,
+    };
+    setMember([...member, newmessage]);
+  };
+  const onPlay = async () => {
+    setTypeRoom("play");
+  };
+  const onAnswer = (name_player, question_id, is_true, remaining_time) => {
+    if (token_host === token_me) {
+      if (is_true) {
+        let newMember = [...member],
+          current_submit_member = newMember.findIndex(
+            (x) => x.name_player === name_player
+          ),
+          current_question_index = data.findIndex(
+            (x) => x.q_id === question_id
+          );
+
+        console.log(current_question_index);
+        let ratio =
+          (remaining_time + 1) / data[current_question_index].num_of_second;
+
+        newMember[current_submit_member].score += Math.floor(
+          data[current_question_index].score * ratio
+        );
+        setMember(newMember);
+      }
+
+      let newSubmit = {
+        name_player: name_player,
+      };
+      setSubmit([...submit, newSubmit]);
+    }
+  };
+  const onNext = (index_next_ques) => {
+    setIndexQues(index_next_ques);
+  };
+  const onScoreBoard = (name_player, rank, score) => {
+    if (name_player === token_me) {
+      setRankScore([rank, score]);
+      // setTypeRender(4);
+    }
+  };
+  const onTimeOut = (name_player) => {
+    if (token_host === token_me) {
+      let newSubmit = {
+        name_player: name_player,
+      };
+      setSubmit([...submit, newSubmit]);
+    }
+  };
+  switch (tmp_message.type_action) {
+    case "append":
+      onAppend(tmp_message.name_player, tmp_message.avatar);
+      break;
+    case "delete":
+      onDelete(tmp_message.name_player);
+      break;
+    case "play":
+      onPlay();
+      break;
+    case "answer":
+      onAnswer(
+        tmp_message.name_player,
+        tmp_message.question_id,
+        tmp_message.is_true,
+        tmp_message.remaining_time
+      );
+      break;
+    case "next":
+      onNext(tmp_message.index_next_ques);
+      break;
+    case "score_board":
+      onScoreBoard(
+        tmp_message.name_player,
+        tmp_message.rank,
+        tmp_message.score
+      );
+      break;
+    case "timeout":
+      onTimeOut(tmp_message.name_player);
+      break;
+    default:
+      break;
+  }
+};
+
+let data = [];
 
 const Room = (props) => {
-    const location = useLocation();
-    let quiz_info = {
-        title: location.state.quiz_info.title,
-        list_question: location.state.quiz_info.list_question,
-        list_option: location.state.quiz_info.list_option,
+  const location = useLocation();
+  const [typeRoom, setTypeRoom] = useState("wait");
+  const [member, setMember] = useState([]);
+  const memberRef = useRef(member);
+
+  useEffect(() => {
+    memberRef.current = member;
+  });
+
+  const [submit, setSubmit] = useState([]);
+  const submitRef = useRef(submit);
+  useEffect(() => {
+    submitRef.current = submit;
+  });
+  const [rank_n_score, setRankScore] = useState([]);
+  const [index_ques, setIndexQues] = useState(-1);
+  let quiz_info = {
+    title: location.state.quiz_info.title,
+    list_question: location.state.quiz_info.list_question,
+    list_option: location.state.quiz_info.list_option,
+  };
+  const client = new W3CWebSocket(
+    "ws://127.0.0.1:8000/ws/wait/" + location.state.quiz_info.pin + "/"
+  );
+  const navigate = useNavigate();
+  useEffect(() => {
+    const handleTabClose = (event) => {
+      event.preventDefault();
+
+      console.log("beforeunload event triggered");
+
+      return (event.returnValue = "Are you sure you want to exit?");
+    };
+
+    const handleKickPlayer = () => {
+      let s =
+        '{ "type_action": "delete","name_player": "' +
+        location.state.my_token +
+        '", "avatar": ""}';
+
+      client.send(s);
+    };
+
+    window.addEventListener("beforeunload", handleTabClose);
+    window.addEventListener("unload", handleKickPlayer);
+    client.onopen = () => {
+      console.log("newmessage");
+      console.log("WebSocket Client Connected");
+      if (location.state.my_token !== location.state.quiz_info.token_host) {
+        let s =
+          '{ "type_action": "append", "name_player": "' +
+          location.state.my_token +
+          '", "avatar": ""}';
+
+        client.send(s);
+      }
+    };
+
+    for (let i = 0; i < location.state.quiz_info.list_question.length; i++) {
+      data.push({
+        num_of_second: location.state.quiz_info.list_question[i].num_of_second,
+        score: location.state.quiz_info.list_question[i].score,
+        q_id: location.state.quiz_info.list_question[i].id,
+        question: location.state.quiz_info.list_question[i].description,
+        options: location.state.quiz_info.list_option.filter(
+          (x) => x.question === location.state.quiz_info.list_question[i].id
+        ),
+      });
     }
-    console.log(quiz_info)
+    client.onmessage = (message) => {
+      memberHandler(
+        message,
+        location.state.quiz_info.pin,
+        quiz_info,
+        location.state.my_token,
+        location.state.quiz_info.token_host,
+        navigate,
+        client,
+        memberRef.current,
+        setMember,
+        submitRef.current,
+        setSubmit,
+        setTypeRoom,
+        data,
+        setIndexQues,
+        setRankScore
+      );
+    };
+
+    return () => {
+      console.log("BAO PRO");
+      window.removeEventListener("beforeunload", handleTabClose);
+      window.removeEventListener("unload", handleKickPlayer);
+      client.close();
+    };
+  }, []);
+  if (typeRoom === "wait") {
     return (
-        <div class="room-body">
-            {<WaitingRoom quiz_info={quiz_info} token_me={location.state.my_token} token_host={location.state.quiz_info.token_host} PIN={location.state.quiz_info.pin}/>}  
-        </div>
+      <div class="room-body">
+        {
+          <WaitingRoom
+            setTypeRoom={setTypeRoom}
+            quiz_info={quiz_info}
+            token_me={location.state.my_token}
+            token_host={location.state.quiz_info.token_host}
+            PIN={location.state.quiz_info.pin}
+            client={client}
+            member={member}
+            setMember={setMember}
+          />
+        }
+      </div>
     );
-}
+  } else {
+    return (
+      <div>
+        {
+          <PlayingRoom
+            title={location.state.quiz_info.title}
+            pin={location.state.quiz_info.pin}
+            token_me={location.state.my_token}
+            token_host={location.state.quiz_info.token_host}
+            data={data}
+            // old_member_size={location.state.old_member_size}
+            client={client}
+            setSubmit={setSubmit}
+            index_ques={index_ques}
+            rank_n_score={rank_n_score}
+            member={member}
+            setMember={setMember}
+            submit={submit}
+          />
+        }
+      </div>
+    );
+  }
+};
 
 export default Room;
